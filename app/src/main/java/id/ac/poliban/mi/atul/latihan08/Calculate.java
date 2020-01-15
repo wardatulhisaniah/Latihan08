5e56b090d5e1fc9745cb123e53cca9b54089032e
wardatulhisaniah <wardatulhisaniah.25@gmail.com>
2020-01-15 14:22:15 +0900

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/atul/latihan08/Calculate.java
@@ -0,0 +1,39 @@
+package id.ac.poliban.mi.atul.latihan08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Calculate extends AppCompatActivity {
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate);
+
+        int fno = getIntent().getIntExtra("fno", 1);
+        int sno = getIntent().getIntExtra("sno", 1);
+        int big, small;
+        int remain;
+
+        if(fno>sno){
+            big = fno;
+            small = sno;
+        }
+        else {
+            big = sno;
+            small = fno;
+        }
+
+        while ((remain = big % small) != 0){
+            big = small;
+            small = remain;
+        }
+
+        TextView tvResult = findViewById(R.id.tvResult);
+        tvResult.setText(String.format("GCF = %3d", small));
+    }
+}
